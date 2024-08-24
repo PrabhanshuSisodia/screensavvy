d40ba5f53a2bf6b2cd1b2d66c2172db519b362f4
Prabhanshu Sisodia <psisodia2001@gmail.com>
2024-08-25 04:20:31 +0530

    Updated movie details

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import { ScrollToTop } from './components';
+import { ScrollToTop } from "./components";
 
 import './index.css';
 import App from './App';
-
-
+        
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <ScrollToTop />
-    <App />
+      <ScrollToTop />
+      <App />
     </Router>
   </React.StrictMode>
 );
-
-
